2aac8d0f2bd3873ecd26511fe659a35a06469e3f
Steve Polito <info@stevepolitodesign.com>
2020-05-10 16:12:29 -0400

    Write failing test.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
-    get pages_root_url
+    get root_path
     assert_response :success
   end
-
 end
