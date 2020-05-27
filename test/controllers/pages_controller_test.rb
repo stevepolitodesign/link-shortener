68c373eabce3e338c0807f4bba6cf2788395b055
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-05-27 10:29:29 +0200

    Fix test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_path
-    assert false
     assert_response :success
   end
 end
