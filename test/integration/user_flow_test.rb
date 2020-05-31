0a803cc31d79ef40c87992eed9fc0823ef6ef4e9
Steve Polito <info@stevepolitodesign.com>
2020-05-31 14:54:56 -0400

    Begin to write user integration tests.

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -1,12 +1,30 @@
 require 'test_helper'
 
 class UserFlowTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   def setup
     @user = users(:one)
   end
 
   test "should register" do
-    flunk
+    get new_user_registration_path
+    assert_select "input[type='email']"
+    assert_select "input[type='password']", 2
+    assert_difference("User.count", 1) do
+      assert_emails 1 do
+        post user_registration_path, params: { user: { email: "uniqe@example.com", password: "password", password_confirmation: "password" } }
+      end
+    end
+    assert_redirected_to root_path
+  end
+
+  test "should sign in" do
+    get new_user_session_path
+    assert_select "input[type='email']"
+    assert_select "input[type='password']"
+    post user_session_path, params: { user: { email: @user.email, password: "password" } }
+    assert_redirected_to root_path
   end
 
   test "should update account" do
