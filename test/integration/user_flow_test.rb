2d3fe4a212d5f1560fd049ad5baa62349e7ae52a
Steve Polito <info@stevepolitodesign.com>
2020-05-31 14:27:43 -0400

    Add blank user flow tests.

---
+++ test/integration/user_flow_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UserFlowTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:one)
+  end
+
+  test "should register" do
+    flunk
+  end
+
+  test "should update account" do
+    flunk
+  end
+
+  test "should cancel account" do
+    flunk
+  end
+
+  test "should reset password" do
+    flunk
+  end
+
+  test "should have navigation for anonymous user" do
+    flunk
+  end
+
+  test "should have navigation for authenticated user" do
+    flunk
+  end
+end
